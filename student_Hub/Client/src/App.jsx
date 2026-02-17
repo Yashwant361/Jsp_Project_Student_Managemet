7a561fb3cae060b2cc2a16d403fabed0d36d6dcf
yashwantray361 <yashwantray361@gmail.com>
2026-02-17 20:25:10 +0530

    update app.jsx

--- student_Hub/Client/src/App.jsx
+++ student_Hub/Client/src/App.jsx
@@ -4,10 +4,11 @@ import Login from './pages/Login'
 import Profile from './pages/Profile'
 import Signup from './pages/Signup'
 import {Toaster} from 'react-hot-toast'
-// import Navbar from './component/Navbar'
-import { useSelector } from 'react-redux'
+import Navbar from './components/Navbar'
+import { useUser } from './context/userContext'
+
 const App = () => {
-  const {isLogin}=useSelector((state)=>state.user);
+  const {isLogin} =useUser()
   const token=localStorage.getItem('token');
   return (
     <BrowserRouter>
