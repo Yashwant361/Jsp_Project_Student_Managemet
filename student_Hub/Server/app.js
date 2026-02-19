188e382c8cc57782f146158893b2d286bb1ccf57
yashwantray361 <yashwantray361@gmail.com>
2026-02-19 19:22:58 +0530

    add app.js

--- student_Hub/Server/app.js
+++ student_Hub/Server/app.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const { configDotenv } = require('dotenv');
 const connectdb = require('./dbConnection/db');
 const stdRouter = require('./routes/stdRouter');
+const stdSubRouter = require('./routes/stdSubRouter');
 
 const app = express();
 
@@ -13,13 +14,16 @@ connectdb();
 app.use(cors())
 app.use(express.json());
 
-// std Router (mount ONCE)
-app.use('/api/std', stdRouter);
-
 // server testing api
 app.get('/', (req, res) => {
     return res.json({ message: 'Studen_Hub server at works' });
 });
+
+// std Router (mount ONCE)
+app.use('/api/std', stdRouter);
+
+app.use('/api/std/subject',stdSubRouter);
+
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
