927f7279bf27ee6bd04188eec6412c10cfa350d9
yashwantray361 <yashwantray361@gmail.com>
2026-02-19 19:12:12 +0530

    update Navbar

--- student_Hub/Client/src/components/Navbar.jsx
+++ student_Hub/Client/src/components/Navbar.jsx
@@ -5,40 +5,54 @@ import { useUser } from '../context/userContext';
 
 
 const Navbar = () => {
-    const navigate=useNavigate();
-    const {setUserDetails,setIsLogin} =useUser()
-    const handleLogout=()=>{
+    const navigate = useNavigate();
+    const { setUserDetails, setIsLogin } = useUser()
+    const handleLogout = () => {
         localStorage.removeItem('token');
         toast.success('logout successfully');
-        setUserDetails({name:"",email:"",age:"",role:""});
+        setUserDetails({ name: "", email: "", age: "", role: "" });
         setIsLogin(false);
         navigate('/login');
     }
- return (
-  <nav className="bg-white dark:bg-gray-900 shadow-md px-6 py-4 flex justify-between items-center transition duration-300">
+    return (
+        <nav className="bg-white dark:bg-gray-900 shadow-md px-6 py-4 flex justify-between items-center transition duration-300">
 
-    <h1 className="text-xl font-bold text-gray-800 dark:text-white">
-      Student Profile
-    </h1>
-    <div className="flex items-center space-x-6">
-      
-      <Link
-        to="/profile"
-        className="text-gray-700 dark:text-gray-200 hover:text-blue-500 transition"
-      >
-        Profile
-      </Link>
+            <h1 className="text-xl font-bold text-gray-800 dark:text-white">
+                Student Profile
+            </h1>
+            <div className="flex items-center space-x-6">
 
-      <button
-        onClick={handleLogout}
-        className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition duration-200 shadow-md"
-      >
-        Logout
-      </button>
+                <Link
+                    to="/subject"
+                    className="text-gray-700 dark:text-gray-200 hover:text-blue-500 transition"
+                >
+                    Subjet
+                </Link>
 
-    </div>
-  </nav>
-);
+                {/* <Link
+                    to="/subject"
+                    className="relative hover:text-yellow-400 transition duration-300 after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-yellow-400 after:transition-all after:duration-300 hover:after:w-full"
+                >
+                    Subject
+                </Link> */}
+
+                <Link
+                    to="/profile"
+                    className="text-gray-700 dark:text-gray-200 hover:text-blue-500 transition"
+                >
+                    Profile
+                </Link>
+
+                <button
+                    onClick={handleLogout}
+                    className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition duration-200 shadow-md"
+                >
+                    Logout
+                </button>
+
+            </div>
+        </nav>
+    );
 
 }
 
