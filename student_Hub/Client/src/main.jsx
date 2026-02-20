3d3fb5144a1a852bfcb5d9248ecf9efc30175b55
yashwantray361 <yashwantray361@gmail.com>
2026-02-20 23:25:02 +0530

    fix main.js

--- student_Hub/Client/src/main.jsx
+++ student_Hub/Client/src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import { StrictMode } from 'react'
+import React ,{ StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { UserWrapper } from './context/userContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <UserWrapper>
+    
      <App />
+      
   </UserWrapper>
     
 )
