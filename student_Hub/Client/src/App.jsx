36594a4c2b42c92283a35d9e06b83ff55559bfa6
yashwantray361 <yashwantray361@gmail.com>
2026-02-24 23:57:55 +0530

    fix app.js

--- student_Hub/Client/src/App.jsx
+++ student_Hub/Client/src/App.jsx
@@ -1,29 +1,39 @@
 import React from 'react'
-import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
 import Signup from './pages/Signup'
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
 import Navbar from './components/Navbar'
 import { useUser } from './context/userContext'
 import Subjects from './pages/Subjects'
+import ForgetPassword from './pages/ForgetPassword'
+import ResetPassword from './pages/ResetPassword'
 
 const App = () => {
-  const {isLogin} =useUser()
-  const token=localStorage.getItem('token');
+  const { isLogin } = useUser()
+  const token = localStorage.getItem('token');
   return (
     <BrowserRouter>
-          {isLogin && <Navbar/>}
-        <Toaster></Toaster>
-        <Routes>
-          <Route path='/' element={token ? <Navigate to='/profile' /> : <Navigate to='/login' />} ></Route>
-          <Route path='/login' element={<Login/>}> </Route>
-          <Route path='/profile' element={<Profile/>} ></Route>
-          <Route path='/signup' element={<Signup />} ></Route>
-          <Route path='/subject' element={<Subjects/>} ></Route>
-
-        </Routes>
+      {isLogin && <Navbar />}
+      <Toaster></Toaster>
+      <Routes>
+        <Route path='/' element={
+          token ? <Navigate to='/profile' />
+            : <Navigate to='/login' />
+        } />
+        <Route path="/forget-password" element={<ForgetPassword />} />
         
+        <Route path='/reset-password/:token' element={<ResetPassword/>} ></Route>
+        
+        <Route path='/login' element={<Login />}> </Route>
+        <Route path='/profile' element={<Profile />} ></Route>
+        <Route path='/signup' element={<Signup />} ></Route>
+        <Route path='/subject' element={<Subjects />} ></Route>
+
+
+      </Routes>
+
     </BrowserRouter>
   )
 }
